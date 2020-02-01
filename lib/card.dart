637ff88288accc9d4a91ee4cc084caf23575f04c
Jayas <34701149+jayasio@users.noreply.github.com>
2020-02-01 12:54:05 +0100

    Card layout

---
+++ lib/card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Expanded(
+        child: Card(
+          color: Color.fromARGB(255, 212, 238, 255),
+          margin: EdgeInsets.all(8.0),
+          elevation: 2.0,
+          child: Column(),
+        ),
+      );
+}
